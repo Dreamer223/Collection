7d3f46e038eec3aded8d6f4c4c18b425080afb11
Dreamer223 <drackfil@gmail.com>
2024-03-15 00:09:12 +0300

    Done!

--- src/main/java/JDK/Employee.java
+++ src/main/java/JDK/Employee.java
@@ -1,2 +1,63 @@
-package JDK;public class Employee {
+package JDK;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private int tabNum;
+    private String phoneNum;
+    private String name;
+    private int experience;
+
+    public Employee(int tabNum, String phoneNum, String name, int experience) {
+        this.tabNum = tabNum;
+        this.phoneNum = phoneNum;
+        this.name = name;
+        this.experience = experience;
+        EmployeeHandbook.addEmployeeToHandbook(this);
+
+
+    }
+
+    public int getTabNum() {
+        return tabNum;
+    }
+
+    public void setTabNum(int tabNum) {
+        this.tabNum = tabNum;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "tabNum=" + tabNum +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", name='" + name + '\'' +
+                ", experience=" + experience +
+                '}';
+    }
 }
--- src/main/java/JDK/EmployeeHandbook.java
+++ src/main/java/JDK/EmployeeHandbook.java
@@ -1,2 +1,29 @@
-package JDK;public class EmployeeHandbook {
+package JDK;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeHandbook {
+    private static List<Employee> employeeHandbookList;
+
+    public EmployeeHandbook() {
+        employeeHandbookList = new ArrayList<>();
+    }
+
+    public void findEmployeeByExperience(int experience){
+        employeeHandbookList.stream().filter(e -> e.getExperience() == experience).forEach(System.out::println);
+    }
+
+    public void findEmployeeByPhoneNum(String phoneNum){
+        employeeHandbookList.stream().filter(e -> e.getPhoneNum().equals(phoneNum)).forEach(System.out::println);
+    }
+
+    public void findEmployeeByTabNum(int tabNum){
+        employeeHandbookList.stream().filter(e -> e.getTabNum() == tabNum).forEach(System.out::println);
+
+    }
+
+    public static void addEmployeeToHandbook(Employee employee){
+        employeeHandbookList.add(employee);
+    }
 }
